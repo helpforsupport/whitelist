31691af65fb76e096ad257ca7a8bffa753e5a488
helpforsupport <56797207+helpforsupport@users.noreply.github.com>
2020-05-09 03:46:03 -0400

    Update server.php

--- server.php
+++ server.php
@@ -8,20 +8,8 @@ $hwidTypes = array("Syn-Fingerprint", "Exploit-Guid", "Proto-User-Identifier", "
 
 $database = array(
 	array(
-		"key" => "key_sentinel",
-		"hwid" => "4700A310329499CFC0BDFFEC717036D9",
-	),
-	array(
-		"key" => "key_sirhurt",
-		"hwid" => "81ECAE2DD1AAADA537121E61D5C0280C"
-	),
-	array(
-		"key" => "key_protosmasher",
-		"hwid" => "506130F68828AC28374C4413C2C660D1F29C056238E8268663312B17BF7D0296"
-	),
-	array(
 		"key" => "key_synapse",
-		"hwid" => "7a0ac74fba20e1903d358ae736298e61fce5b58c2c7924a8117153f7a2fcb03d5380d72e511d1af0ea2288bc26c1b1b5f1cf64e32c8ed79301a113ab5b528bf5"
+		"hwid" => "cd6075d0ddbd00b9c3120f0c133987550ee7933e0592b7b15a0596500a422c99da2844e79cf7765db3465004891de4fd78d416ab284c5b29ad528feb3c31e9ba"
 	),
 );
 
